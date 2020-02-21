d358074fa0e2a21dd0f10b4b7d0eb16f8b7fd94f
raymond.sutrisno <raymond.sutrisno@slalom.com>
2020-02-21 14:20:12 -0600

    basic express server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
 
-console.log("Hello, World!");
+app.get('/', (request, response) => {
+  console.log("Received request: ", request);
+  response.sendStatus(200);
+})
 
+app.listen(port, (err) => {
+  if (err) {
+    return console.error('Error! Flux capacitor broke :C ', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
