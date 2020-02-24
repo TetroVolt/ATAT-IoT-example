aad159311563a3e931ace2a6dc4616392fcf137e
raymond.sutrisno <raymond.sutrisno@slalom.com>
2020-02-24 10:18:10 -0600

    added socketio

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+const PORT = 3000
 
 app.get('/', (request, response) => {
   console.log("Received request: ", request);
   response.sendStatus(200);
 })
 
-app.listen(port, (err) => {
-  if (err) {
-    return console.error('Error! Flux capacitor broke :C ', err)
-  }
-
-  console.log(`server is listening on ${port}`)
+app.get('/health', (request, response) => {
+  console.log("Received health check from: ", request);
+  response.sendStatus(200);
 })
+
+io.on('connection', function(socket) {
+  console.log("Socket connection received.");
+  socket.send("Hello, World");
+})
+
+http.listen(PORT, function(){
+  console.log('listening on *:3000');
+});
