378426c3f6e7396c81026cb2b189ba5ba95c7f58
david-slalom <david.tompkins@slalom.com>
2020-02-24 10:17:28 -0600

    C File

--- IMPINJ-Reader/src/main.c
+++ IMPINJ-Reader/src/main.c
@@ -25,34 +25,131 @@
  */ 
 #include <stdio.h>
 #include <curl/curl.h>
- 
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#include <arpa/inet.h>
+
+#define PORT "3000" // the port client will be connecting to 
+
+#define MAXDATASIZE 100 // max number of bytes we can get at once 
+
+
+    int getaddrinfo(const char *node,     // e.g. "www.example.com" or IP
+                    const char *service,  // e.g. "http" or port number
+                    const struct addrinfo *hints,
+                    struct addrinfo **res);
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+
+int main(int argc, char *argv[])
+{
+    int sockfd, numbytes;  
+    char buf[MAXDATASIZE];
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+
+    if (argc != 2) {
+        fprintf(stderr,"usage: client hostname\n");
+        exit(1);
+    }
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and connect to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("client: socket");
+            continue;
+        }
+
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("client: connect");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "client: failed to connect\n");
+        return 2;
+    }
+
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+            s, sizeof s);
+    printf("client: connecting to %s\n", s);
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+        perror("recv");
+        exit(1);
+    }
+
+    buf[numbytes] = '\0';
+
+    printf("client: received '%s'\n",buf);
+
+    close(sockfd);
+
+    return 0;
+}
+
+/*
 int main(void)
 {
   CURL *curl;
   CURLcode res;
  
-  /* In windows, this will init the winsock stuff */ 
+  // In windows, this will init the winsock stuff 
   curl_global_init(CURL_GLOBAL_ALL);
  
-  /* get a curl handle */ 
+  // get a curl handle  
   curl = curl_easy_init();
   if(curl) {
-    /* First set the URL that is about to receive our POST. This URL can
-       just as well be a https:// URL if that is what should receive the
-       data. */ 
+    // First set the URL that is about to receive our POST. This URL can
+      // just as well be a https:// URL if that is what should receive the
+       //data. 
     curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:3000/");
     
-    /* Perform the request, res will get the return code */ 
+    // Perform the request, res will get the return code 
     res = curl_easy_perform(curl);
     
-    /* Check for errors */ 
+    // Check for errors 
     if(res != CURLE_OK)
       fprintf(stderr, "curl_easy_perform() failed: %s\n",
               curl_easy_strerror(res));
  
-    /* always cleanup */ 
+    // always cleanup  
     curl_easy_cleanup(curl);
   }
   curl_global_cleanup();
   return 0;
 }
+*/
